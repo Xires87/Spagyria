f6330f1ec41eabeda820fd498926a6ff5e9e2540
Xires87 <frycmod@gmail.com>
2023-04-29 12:48:46 +0200

    3.1.0 (without textures)

--- src/main/java/net/fryc/threepotions/loot/ModLootTables.java
+++ src/main/java/net/fryc/threepotions/loot/ModLootTables.java
@@ -3,11 +3,14 @@ package net.fryc.threepotions.loot;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.fryc.threepotions.items.ModItems;
 import net.minecraft.loot.LootPool;
-import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.condition.*;
+import net.minecraft.loot.context.LootContext;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.function.*;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.predicate.entity.EntityFlagsPredicate;
+import net.minecraft.predicate.entity.EntityPredicate;
 import net.minecraft.util.Identifier;
 
 public class ModLootTables {
@@ -19,6 +22,14 @@ public class ModLootTables {
             = new Identifier("minecraft", "entities/guardian");
     private static final Identifier ELDER_GUARDIAN
             = new Identifier("minecraft", "entities/elder_guardian");
+    private static final Identifier POLAR_BEAR
+            = new Identifier("minecraft", "entities/polar_bear");
+    private static final Identifier SILVERFISH
+            = new Identifier("minecraft", "entities/silverfish");
+    private static final Identifier BAT
+            = new Identifier("minecraft", "entities/bat");
+    private static final Identifier DOLPHIN
+            = new Identifier("minecraft", "entities/dolphin");
 
     public static void modifyLoot(){
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
@@ -36,9 +47,10 @@ public class ModLootTables {
             if(WITHER_SKELETON.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.10f))
+                        .conditionally(RandomChanceWithLootingLootCondition.builder(0.10f, 0.07f))
                         .with(ItemEntry.builder(ModItems.WITHER_BONE))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build())
+                        .apply(LootingEnchantLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f)).withLimit(2).build());
                 tableBuilder.pool(poolBuilder.build());
             }
         });
@@ -47,7 +59,7 @@ public class ModLootTables {
             if(GUARDIAN.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.025f))
+                        .conditionally(RandomChanceWithLootingLootCondition.builder(0.025f, 0.01f))
                         .with(ItemEntry.builder(ModItems.RED_CRYSTALS))
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
                 tableBuilder.pool(poolBuilder.build());
@@ -61,6 +73,55 @@ public class ModLootTables {
                         .conditionally(RandomChanceLootCondition.builder(1.0f))
                         .with(ItemEntry.builder(ModItems.RED_CRYSTALS))
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
+                tableBuilder.pool(poolBuilder.build());
+            }
+        });
+
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if(POLAR_BEAR.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceWithLootingLootCondition.builder(0.63f, 0.12f))
+                        .with(ItemEntry.builder(ModItems.POLAR_BEAR_LIVER))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build())
+                        .apply(FurnaceSmeltLootFunction.builder()
+                                .conditionally(EntityPropertiesLootCondition.builder(LootContext.EntityTarget.THIS, EntityPredicate
+                                        .Builder.create().flags(EntityFlagsPredicate.Builder.create().onFire(true).build()))).build());
+                tableBuilder.pool(poolBuilder.build());
+            }
+        });
+
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if(SILVERFISH.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceWithLootingLootCondition.builder(0.14f, 0.1f))
+                        .with(ItemEntry.builder(ModItems.SILVERFISH_SKIN))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build())
+                        .apply(LootingEnchantLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f)).withLimit(2).build());
+                tableBuilder.pool(poolBuilder.build());
+            }
+        });
+
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if(BAT.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceWithLootingLootCondition.builder(0.30f, 0.13f))
+                        .with(ItemEntry.builder(ModItems.BAT_EYE))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build())
+                        .apply(LootingEnchantLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f)).withLimit(2).build());
+                tableBuilder.pool(poolBuilder.build());
+            }
+        });
+
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if(DOLPHIN.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceWithLootingLootCondition.builder(0.22f, 0.15f))
+                        .with(ItemEntry.builder(ModItems.DOLPHIN_FIN))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
                 tableBuilder.pool(poolBuilder.build());
             }
         });
